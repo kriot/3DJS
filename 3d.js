52a93a1361363262b985c1d52f0fde3b15c0bc71
kriot <gosha-bor@yandex.ru>
2014-02-07 15:24:03 +0000

    matrix

--- 3d.js
+++ 3d.js
@@ -8,8 +8,7 @@ function Engine3d()
 	this.drawData = new Array();
 	this.eye;
 	this.mouse = new Point2(0,0);
-	this.view = new Point3(0,0,0);
-	this.viewangle = 0;
+	this.mat;
 	this.Init = function(canID, w, h, UpdateFunc, MouseFunct,KeyFunct, eye)
 	{
 		this.width = w;
@@ -19,6 +18,7 @@ function Engine3d()
 		this.can.addEventListener("mousemove", MouseFunct);
 		this.context = this.can.getContext("2d");
 		this.Update = UpdateFunc;
+		this.LoadIdentity();
 	}
 	this.Redraw = function()
 	{
@@ -26,7 +26,9 @@ function Engine3d()
 		this.context.beginPath();
 		for(i = 0; i < this.drawData.length; i++)
 		{
-			this.DrawLine(this.Projection(this.drawData[i].p1.AddPoint(this.view)), this.Projection(this.drawData[i].p2.AddPoint(this.view)));
+			p1 = this.Calc(this.drawData[i].p1);
+			p2 = this.Calc(this.drawData[i].p2);
+			this.DrawLine(p1,p2);
 		}
 		this.context.stroke();
 	}
@@ -35,14 +37,27 @@ function Engine3d()
 		this.drawData.push(line);
 		return this.drawData.length-1;
 	}
+	this.Calc = function(p)
+	{
+		p2 = p.MulMatrix(this.mat);
+		p2.Div(p2.w);
+		return p2;
+	}
 	this.DrawLine = function(p1, p2)
 	{
 		this.context.moveTo(p1.x+this.width/2,this.height/2 - p1.y);
 		this.context.lineTo(p2.x+this.width/2,this.height/2 - p2.y);
 	}
-	this.Projection = function(p)
+	this.LoadIdentity = function()
 	{
-		return new Point2(p.x*this.eye/(this.eye+p.z), p.y*this.eye/(this.eye+p.z));
+		this.mat = new Matrix();
+		for(i=0;i<4;i++)
+			for(j=0;j<4;j++)
+				this.mat.data[i][j] = (i==j?1:0);
+	}
+	this.Use = function(m)
+	{
+		this.mat = this.mat.Mul(m);
 	}
 }
 
@@ -56,13 +71,60 @@ function Point3(x,y,z)
 	this.x = x;
 	this.y = y;
 	this.z = z;
+	this.w = 1;
 	this.AddPoint = function(p2)
 	{
 		return new Point3(this.x+p2.x, this.y+p2.y, this.z+p2.z);
+	}
+	this.MulMatrix = function(m)
+	{
+		ret = new Point3(0,0,0);
+		ret.x += m.data[0][0]*this.x + m.data[1][0]*this.y + m.data[2][0]*this.z + m.data[3][0]*this.w;
+		ret.y += m.data[0][1]*this.x + m.data[1][1]*this.y + m.data[2][1]*this.z + m.data[3][1]*this.w;
+		ret.z += m.data[0][2]*this.x + m.data[1][2]*this.y + m.data[2][2]*this.z + m.data[3][2]*this.w;
+		ret.w += m.data[0][3]*this.x + m.data[1][3]*this.y + m.data[2][3]*this.z + m.data[3][3]*this.w;
+		return ret;	
+	}
+	this.Div = function(k)
+	{
+		this.x/=k;
+		this.y/=k;
+		this.z/=k;
+		this.w/=k;
 	}
 }
 function Point2(x,y)
 {
 	this.x = x;
 	this.y = y;
+}
+function Matrix()
+{
+	this.data = new Array(4);
+	for(i = 0;i<4;i++)
+		this.data[i] = new Array(4);
+	this.Mul = function(m2)
+	{
+		ret = new Matrix();
+		for(i = 0; i< 4;i++)
+			for(j = 0; j <4; j++)
+			{
+				ret.data[i][j] = 0;
+				for(k = 0; k<4;k++)
+					ret.data[i][j] += this.data[i][k]*m2.data[k][j];
+			}
+		return ret
+	}
+	this.MakeT = function(p)
+	{
+		this.data = [[1, 0, 0, 0],[0, 1, 0, 0], [0, 0 ,1, 0],[p.x, p.y, p.z, 1]];
+	}
+	this.MakeS = function(s)
+	{
+		this.data = [[s.x, 0, 0, 0],[0, s.y, 0, 0], [0, 0 ,s.z, 0],[0, 0, 0, 1]];
+	}
+	this.MakeP = function(r)
+	{
+		this.data = [[1, 0, 0, 0],[0, 1, 0, 0], [0, 0 , 0, r],[0, 0, 0, 1]];
+	}
 }
--- game.js
+++ game.js
@@ -4,16 +4,27 @@ var l;
 function Load()
 {
 	eng = new Engine3d();
-	eng.Init("canv", 640, 480, Update,Mouse,1000);
+	eng.Init("canv", 640, 480, Update,Mouse,Kbd,1000);
 	for(i = 0;i< 10;i++)
 	{
 		eng.AddLine(new Line(new Point3(i*10,-20,0), new Point3(i*10, -20, 2000)));
 	}
+	eng.AddLine(new Line(new Point3(50,50,0), new Point3(50, 50, 200)));
 	setInterval(Update, 1000/20);
 }
 
 function Update()
 {
+	eng.LoadIdentity();
+	mt = new Matrix();
+	mt.MakeT(new Point3(50,50,50));
+	ms = new Matrix();
+	ms.MakeS(new Point3(2,2,2));
+	mp = new Matrix();
+	mp.MakeP(1/1000);
+	eng.Use(mp);
+	//eng.Use(ms);
+	//eng.Use(mt);
 	for(i = 0;i<10;i++)
 	{
 		eng.drawData[i].p1.y = -eng.mouse.y/4;
@@ -25,4 +36,7 @@ function Update()
 function Mouse(event)
 {
 	eng.mouse = new Point2(event.clientX, event.clientY);
+}
+function Kbd()
+{
 }
